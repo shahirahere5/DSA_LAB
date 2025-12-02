c3ffb7b9ac0688bc19654ff8f8ceb19d03965528
shahirahere5 <139383955+shahirahere5@users.noreply.github.com>
2025-12-02 13:15:50 +0500

    Create kmp.cpp

---
+++ algorithms/kmp.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void computeLPS(string pattern, int lps[]) {
+    int m = pattern.length();
+    int len = 0;
+    lps[0] = 0;
+
+    int i = 1;
+    while (i < m) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void KMPsearch(string text, string pattern) {
+    int n = text.length();
+    int m = pattern.length();
+
+    int lps[m];
+    computeLPS(pattern, lps);
+
+    int i = 0;
+    int j = 0;
+
+    while (i < n) {
+        if (pattern[j] == text[i]) {
+            i++;
+            j++;
+        }
+
+        if (j == m) {
+            cout << "Pattern found at index " << (i - j) << endl;
+            j = lps[j - 1];
+        }
+
+        else if (i < n && pattern[j] != text[i]) {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i++;
+        }
+    }
+}
+
+int main() {
+    string text = "ABABDABACDABABCABAB";
+    string pattern = "ABA";
+
+    KMPsearch(text, pattern);
+
+    return 0;
+}
